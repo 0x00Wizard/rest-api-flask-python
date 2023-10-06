b8d0f36f3fe950130fd80eb7cfa02055b15b6eb9
wizardxxdx <wizardxd@icloud.com>
2023-10-06 01:19:26 -0700

    Create an update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -18,6 +18,23 @@ stores = [
 @app.get("/store")
 def get_store():
     return {"store": stores}
+
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201
+
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    requests_data = request.get_json()
+    for store in stores:
+        if store[name] == name:
+            new_item = {"name": requests_data["name"], "price": requests_data["price"]}
+
 
 
 if __name__ == "__main__":
