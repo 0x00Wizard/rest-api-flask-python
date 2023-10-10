4ef98542284b5fa4f9c71cf4341ba0880feeb6fc
wizardxxdx <wizardxd@icloud.com>
2023-10-10 05:29:37 -0700

    Create an update

---
+++ db.py
@@ -0,0 +1,16 @@
+stores = [
+    {
+        "name": "My Store",
+    }
+]
+
+items = {
+    1: {
+        "name": "Chair",
+        "price": 17.99
+    },
+    2: {
+        "name": "Table",
+        "price": 180.50
+    }
+}
--- main.py
+++ main.py
@@ -1,39 +1,55 @@
+import uuid
 from flask import Flask, request
+from db import items, stores
 
 app = Flask(__name__)
 
-stores = [
-    {
-        "name": "my store",
-        "items": [
-            {
-                "name": "Chair",
-                "price": 15.99
-            }
-        ]
-    }
-]
-
 
 @app.get("/store")
-def get_store():
-    return {"store": stores}
+def get_stores():
+    return {"stores": list(stores.values())}
 
 
 @app.post("/store")
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
-    return new_store, 201
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
 
 
-@app.post("/store/<string:name>/item")
-def create_item(name):
-    requests_data = request.get_json()
+
+
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        return {"message": "Store not found"}, 404
+
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+    return item, 201
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+@app.get("/store/<string:store_id>")
+def get_stores(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return {"messages": "Store not found"}, 404
+
+@app.get("/store/<string>")
+def get_items(name):
     for store in stores:
-        if store[name] == name:
-            new_item = {"name": requests_data["name"], "price": requests_data["price"]}
+        if store["name"] == name:
+            r
+
+
 
 
 
