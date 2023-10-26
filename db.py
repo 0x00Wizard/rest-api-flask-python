8a5b7944782c6b193e482b622d019ccb4bffb88c
wizardxxdx <wizardxd@icloud.com>
2023-10-26 06:49:08 -0700

    my_last update

--- db.py
+++ db.py
@@ -1,16 +1,3 @@
-stores = [
-    {
-        "name": "My Store",
-    }
-]
+stores = {}
+items = {}
 
-items = {
-    1: {
-        "name": "Chair",
-        "price": 17.99
-    },
-    2: {
-        "name": "Table",
-        "price": 180.50
-    }
-}
